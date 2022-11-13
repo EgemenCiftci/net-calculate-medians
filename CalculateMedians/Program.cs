96bf0ef85322ee8286ce4ff1e9ca72c60c7aadce
Egemen Çiftci <egemenciftci@gmail.com>
2022-11-13 12:47:21 +0100

    Upgraded to .NET 7.

--- CalculateMedians/Program.cs
+++ CalculateMedians/Program.cs
@@ -1,19 +1,24 @@
 ﻿/*|||||||||||||||||||||||
 ||||  Egemen Ciftci  ||||
 |||||||||||||||||||||||*/
-var array = new double[] { 5, 2, 1, 4, 3 };
-var medians = CalculateMedians(array);
+double[] array = new double[] { 5, 2, 1, 4, 3 };
+IEnumerable<double> medians = CalculateMedians(array);
 Console.WriteLine(string.Join(" ", medians));
 
 static IEnumerable<double> CalculateMedians(IEnumerable<double> input)
 {
-    // Declare two min heaps
-    var smaller = new PriorityQueue<double, double>(); // The values of this priority queue will be multiplied by -1 to convert it to a max heap.
-    var greater = new PriorityQueue<double, double>();
-
-    foreach (var item in input)
+    if (input == null)
     {
-        var value = -item;
+        throw new ArgumentNullException("input is null");
+    }
+
+    // Declare two min heaps
+    PriorityQueue<double, double> smaller = new(); // The values of this priority queue will be multiplied by -1 to convert it to a max heap.
+    PriorityQueue<double, double> greater = new();
+
+    foreach (double item in input)
+    {
+        double value = -item;
         value = -smaller.EnqueueDequeue(value, value);
 
         if (greater.Count == smaller.Count)
@@ -26,9 +31,6 @@ static IEnumerable<double> CalculateMedians(IEnumerable<double> input)
             greater.Enqueue(value, value);
         }
 
-        if (greater.Count == smaller.Count)
-            yield return (greater.Peek() - smaller.Peek()) / 2;
-        else
-            yield return -smaller.Peek();
+        yield return greater.Count == smaller.Count ? (greater.Peek() - smaller.Peek()) / 2 : -smaller.Peek();
     }
 }
